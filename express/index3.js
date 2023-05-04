e96c2df3016f505a1c1c426411213e809c490454
Shahin Amon <amons@bzz.ch>
2023-05-04 14:52:36 +0000

    login/ogout task solved

--- express/index3.js
+++ express/index3.js
@@ -1,6 +1,7 @@
 import express, { request, response } from "express";
 import lendsRouter from "./routes/lends.js";
 import booksRouter from "./routes/books.js";
+import session from "express-session";
 
 const app = express()
 const port = 3000
@@ -9,11 +10,51 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/lends', lendsRouter);
 app.use('/books', booksRouter);
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {}
+}))
+
+const secretAdminCredentials = {
+    email: "a@gmail.com",
+    password: 123
+}
+
+app.post('/login', (request, response) => {
+    const { email, password } = request.body;
+    const lower_email = email?.toLowerCase();
+    if (lower_email === secretAdminCredentials.email && password === secretAdminCredentials.password){
+        request.session.email = email;
+        return response.status(200).json({ email: request.session.email });
+    }
+    return response.status(401).json({ error: "Invalid Credentials" });
+})
+
+app.get('/verify', function (request, response) {
+    if (request.session.email){
+        return response.status(200).json({ email: request.session.email });
+    }
+    return response.status(401).json({ error: "Nicht eingeloggt!" });
+})
+
+app.delete('/logout', function (request, response) {
+    if (request.session.email){
+        request.session.email = null;
+        return response.status(204).send();
+    }
+    return response.status(401).json({ error: "Nicht eingeloggt!" });
+})
+    
 
 // TODO: 
 // - Bei allen den entsprechenden Status Code mitgeben
 // - Parameter darf nicht leer sein, Code 422 zurückgeben
 // -Evtl. Funktionen auslagern (export function getBookByIsbn(isbn) usw...)
+
+
+
 
 app.listen(port, () => {
     console.log(`Läuft auf Port ${port}`);
